c16a4f9379f984f1b34ad6c825685f4e80a6bcbe
Amandeep Thakur <aman914501@gmail.com>
2022-11-15 10:47:58 +0530

    camera Open

---
+++ app/src/main/java/com/example/mychatbot_1/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.example.mychatbot_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mychatbot_1.ui.MainActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000) // 3000 is the delayed time in milliseconds.
+    }
+}
